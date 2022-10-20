e91d212b124da841b658638a98ce0754604ee32b
Mirco Richter <mirco.richter@campus.tu-berlin.de>
2022-10-20 10:02:52 +0200

    init

---
+++ include/arch/bsp/yellow_led.h
@@ -0,0 +1,6 @@
+#ifndef YELLOW_LED_H
+#define YELLOW_LED_H
+
+void yellow_on(void);
+
+#endif
---
+++ include/config.h
@@ -0,0 +1,50 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/* Falls auf den Boards gearbeitet wird am besten
+ * die nächste Zeile auskommentieren
+ */
+#define __QEMU__
+
+/**
+ * \file config.h
+ *
+ * Enthält defines und static Funktionen zum testen der
+ * Implementierung. Wir tauschen diese Werte/Funktionen beim
+ * Korrigieren zum Testen aus. Bitte fügt hier KEINE weiteren
+ * defines oÄ ein. Ihr könnt diese Werte zum Testen natürlich
+ * auch gerne selbst verändern.
+ */
+
+/* Include-Pfad bitte gegenfalls anpassen */
+#include <kernel/kprintf.h>
+static inline void test_kprintf(void) {
+	/* Nur für Aufgabenblatt 1
+	 * Hier Test Funktion für kprintf schreiben und
+	 * nach dem vollständigen initialisieren aufrufen
+	 */
+	kprintf("kprintf_test\n");
+}
+
+/**
+ * Erst ab Aufgabenblatt 2 relevant
+ */
+
+#ifdef __QEMU__
+/* Werte zum testen unter QEMU */
+#define BUSY_WAIT_COUNTER 10000000
+#else
+/* Werte zum testen auf der Hardware */
+#define BUSY_WAIT_COUNTER 30000
+#endif // __QEMU__
+
+// Wir testen nur mit Werten die durch 2^n darstellbar sind
+#define UART_INPUT_BUFFER_SIZE 128
+
+// Timer Interrupt Interval zum testen in Mikrosekunden
+// Systimer taktet mit 1MHz
+// 1000000 -> 1 Sekunde
+#define TIMER_INTERVAL 1000000
+
+
+#endif // _CONFIG_H_
