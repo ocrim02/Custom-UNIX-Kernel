45bc1fb2149105afb3de4dc29942db0b7dbdf707
Mirco Richter <mirco.richter@campus.tu-berlin.de>
2022-10-26 22:22:31 +0200

    first types set

--- arch/bsp/pl011_uart.c
+++ arch/bsp/pl011_uart.c
@@ -1,9 +1,14 @@
 #include <arch/bsp/pl011_uart.h>
 #include <arch/bsp/yellow_led.h>
+#include <stdarg.h>
+#include <math.h>
 
 #define UART_BASE (0x7E201000 - 0x3F000000)
 #define FR_OFFSET 0x18
 #define DR_OFFSET 0x0
+
+#define MAX_INT_STRING_SIZE 6
+#define MAX_UNSIGNED_INT_STRING_SIZE 5
 //#define LCRH_OFFSET 0x2c
 
 
@@ -47,6 +52,7 @@ BUSY: bussy sending
 CTS: 
 */
 
+//implement FR checks
 void read_uart(void)
 {
     /*unsigned int data = 0;
@@ -62,14 +68,104 @@ void read_uart(void)
         if(data_reg->data == 0x72)
         {
             light_row();
-            write_uart('r');
+            kprintf("Hello I am %i Years old. %u lol", -5000, 60000);
+            //int_to_str(327);
         }
     }
     
 }
 
-
+//implement FR checks
 void write_uart(char character)
 {
     data_reg->data = character;
+}
+
+void kprintf(char* string, ...)
+{
+    
+    va_list ap;
+    va_start(ap, string);
+    
+    char character = *string++;
+    while(character != '\0'){
+        if(character == '%')
+        {
+            character = *string++;
+            if(character == 'i'){
+                int value = va_arg(ap, int);
+                kprintf(int_to_str(value));
+                character = *string++;
+            }
+            if(character == 'u'){
+                unsigned int value = va_arg(ap, unsigned int);
+                kprintf(unsigned_int_to_str(value));
+                character = *string++;
+            }
+            if(character == 's'){
+                char* value = va_arg(ap, char*);
+                kprintf(value);
+                character = *string++;
+            }
+            
+        }
+        write_uart(character);
+        character = *string++;
+    }
+    va_end(ap);
+}
+
+//converts unsigned int to char array
+char* unsigned_int_to_str (unsigned int value)
+{
+    static char string[MAX_UNSIGNED_INT_STRING_SIZE + 1];
+
+    //catch value = 0
+    if(value == 0)
+    {
+        string[0] = '0';
+        string[1] = '\0';
+        return string;
+    }
+
+    int divisor = 1;
+    int len = 0;
+    while(value/divisor != 0){
+        len++;
+        divisor = divisor * 10;
+    }
+    divisor = divisor/10;
+    
+    int digit;
+    for(int i=0; i<=len-1; i++)
+    {
+        digit = value/divisor;
+        string[i] = digit + '0';
+        value = value - digit * divisor;
+        divisor = divisor/10;
+    }
+    string[len] = '\0'; 
+
+    return string;
+}
+
+//converts int to char array
+char* int_to_str (int value)
+{
+    static char string[MAX_INT_STRING_SIZE + 1];
+    int negatives_offset = 0;
+    if(value < 0){
+        string[0] = '-';
+        negatives_offset = 1;
+        value = value * (-1);
+    }
+
+    char* rest = unsigned_int_to_str((unsigned int) value);
+    for(int i=0; i<= MAX_UNSIGNED_INT_STRING_SIZE; i++){
+        string[i + negatives_offset] = rest[i];
+    }
+
+    return string;
+    
+
 }
--- include/arch/bsp/pl011_uart.h
+++ include/arch/bsp/pl011_uart.h
@@ -3,5 +3,8 @@
 
 void read_uart(void);
 void write_uart(char);
+void kprintf(char*, ...);
+char* int_to_str(int);
+char* unsigned_int_to_str(unsigned int);
 
 #endif
